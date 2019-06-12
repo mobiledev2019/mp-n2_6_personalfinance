4f1313daee85e30a608b2c29b10135a51a324a91
tamnntit <thosans1997@gmail.com>
2019-06-13 01:22:49 +0700

    Thêm chức năng xin cấp quyền truy cập vào camera và thư viện

--- app/src/main/java/com/example/personal/AddCAActivity.java
+++ app/src/main/java/com/example/personal/AddCAActivity.java
@@ -1,5 +1,6 @@
 package com.example.personal;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.Notification;
@@ -9,6 +10,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -16,7 +18,9 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -55,8 +59,8 @@ public class AddCAActivity extends AppCompatActivity  {
     static List<String> groups = new ArrayList<String>(Arrays.asList("Ăn uống", "Trang phục","Đi lại","Học tập", "Sức khỏe", "Giải trí", "Nhà cửa", "Nhận lương", "Khác"));
     static List<String> status = new ArrayList<String>(Arrays.asList("Hoàn tất", "Chưa hoàn tất"));
 
-    int REQUEST_CODE_CAMERA = 123;
-    int REQUEST_CODE_FOLDER = 456;
+    final int REQUEST_CODE_CAMERA = 123;
+    final int REQUEST_CODE_FOLDER = 456;
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -177,19 +181,48 @@ public class AddCAActivity extends AppCompatActivity  {
         ibtnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, REQUEST_CODE_CAMERA );
+//
+                ActivityCompat.requestPermissions(AddCAActivity.this, new String[] {
+                    Manifest.permission.CAMERA
+                }, REQUEST_CODE_CAMERA);
             }
         });
 
         ibtnForder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                startActivityForResult(intent, REQUEST_CODE_FOLDER);
+//                Intent intent = new Intent(Intent.ACTION_PICK);
+//                intent.setType("image/*");
+//                startActivityForResult(intent, REQUEST_CODE_FOLDER);
+                ActivityCompat.requestPermissions(AddCAActivity.this, new String[] {
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                }, REQUEST_CODE_FOLDER);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_CAMERA:
+                if(grantResults .length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                     startActivityForResult(intent, REQUEST_CODE_CAMERA );
+                } else {
+                    Toast.makeText(this, "Bạn không cho phép vào camera!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case REQUEST_CODE_FOLDER:
+                if(grantResults .length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/*");
+                    startActivityForResult(intent, REQUEST_CODE_FOLDER);
+                } else {
+                    Toast.makeText(this, "Bạn không cho phép truy cập vào thư viện ảnh!", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
